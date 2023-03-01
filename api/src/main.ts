dadc1db29ba3745fda1ac53356dad3979a7e927d
DibyodyutiMondal <50345677+DibyodyutiMondal@users.noreply.github.com>
2023-03-01 10:11:48 +0530

    write a very minimal web server

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,1 +1,21 @@
-console.log('Hello World');
+import fastify from 'fastify';
+
+const host = '0.0.0.0';
+const port = 9100;
+
+setupServer()
+  .then(server => server.listen({ host, port }))
+  .then(() => {
+    console.log('server started...');
+    console.log('port: ' + port);
+  });
+
+async function setupServer() {
+  const server = fastify();
+
+  server.get('/', async () => {
+    return { 'hello': 'world' };
+  });
+
+  return server;
+}
